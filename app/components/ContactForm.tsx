ddf6c702ac511c1810af254c34642e1b6a85e4be
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-03-25 15:44:21 +0000

    fixed the form to work by not forgetting the name attribute of the input fields.

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -13,7 +13,7 @@ export default function ContactForm() {
     const [success, setSuccess] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        console.log("handling change")
+        console.log(`handling change -> ${e.target.name} : ${e.target.value} ` )
         setFormData({...formData, [e.target.name]: e.target.value});
     }
 
@@ -44,9 +44,9 @@ export default function ContactForm() {
         <h2 className="text-xl font-semibold text-center">Let's Connect!</h2>
         {success && <p className="text-green-500">Message sent successfully!</p>}
         <form onSubmit={handleSubmit} className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <input value={formData.name} onChange={handleChange} type="text" placeholder="Name" className="p-2 bg-gray-700 rounded" required />
-          <input value={formData.email} onChange={handleChange} type="email" placeholder="Email" className="p-2 bg-gray-700 rounded" required />
-          <textarea value={formData.message} onChange={handleChange} placeholder="Message" className="p-2 bg-gray-700 rounded col-span-2" required />
+          <input value={formData.name} onChange={handleChange} type="text" placeholder="Name" className="p-2 bg-gray-700 rounded" required name="name" />
+          <input value={formData.email} onChange={handleChange} type="email" placeholder="Email" className="p-2 bg-gray-700 rounded" required name="email" />
+          <textarea value={formData.message} onChange={handleChange} placeholder="Message" className="p-2 bg-gray-700 rounded col-span-2" required name="message" />
           <button type="submit"  className="col-span-2 px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-500" disabled={isSubmitting}>
             {isSubmitting ? "Sending...": "Send Message"}
           </button>
