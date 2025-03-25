112f4871b14d75ee844ef42e2a7fb8e72f78d287
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-03-25 09:07:48 +0000

    added a component to display messages

---
+++ app/components/Messages.tsx
@@ -0,0 +1,29 @@
+import { sanityClient } from "@/sanity";
+import { useEffect, useState } from "react";
+
+type Message = {
+    _id: string;
+    name: string;
+    email: string;
+    message: string;
+};
+
+export default function Messages() {
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == message] | order(_createdAt desc)`).then(setMessages), []});
+    
+    return (
+        <div className="p-6 bg-gray-800 text-white rounded-lg">
+            <h2 className="text-xl mb-4">Messages</h2>
+            {messages.length === 0 && <p>No Messages yet.</p>}
+            {messages.map(msg => (
+                <div key={msg._id} className="p-3 border-b border-gray-600">
+                    <p><strong>{msg.name}</strong> ({msg.email})</p>
+                    <p>{msg.message}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
--- sanity.ts
+++ sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from "@sanity/client";
 export const sanityClient = createClient({
     projectId: "vh4bzmyq",
     dataset: "production",
-    useCdn: true,
+    useCdn: false,
     apiVersion: "2025-03-24"
 });
