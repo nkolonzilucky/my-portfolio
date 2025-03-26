c426b0938c0c0f14c059698789a2d3c00f866280
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-03-26 18:44:35 +0000

    sanity troubles, debugging in progress.

--- sanity.ts
+++ sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from "@sanity/client";
 export const sanityClient = createClient({
     projectId: "vh4bzmyq",
     dataset: "production",
-    useCdn: false,
+    useCdn: true,
     apiVersion: "2025-03-24"
 });
